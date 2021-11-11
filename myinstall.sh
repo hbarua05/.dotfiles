f4d3c6557c47186a5dbe7c1fb76734ba7185df24
Hrithik Barua <66213858+hbarua05@users.noreply.github.com>
2021-11-11 14:38:25 +0600

    Update myinstall.sh

--- myinstall.sh
+++ myinstall.sh
@@ -21,6 +21,9 @@ sudo apt-get install curl -y
 echo "INSTALLING node"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 command -v nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 nvm ls
 
