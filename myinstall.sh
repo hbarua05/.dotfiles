6634881528463bfb8777f8c92ad265723048da42
Hrithik Barua <hbarua05@gmail.com>
2021-11-11 11:58:46 +0600

    install script

---
+++ myinstall.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# INSTALL SCRIPT FOR UBUNTU
+
+cd
+echo "HELLO ${whoami}"
+echo "INSTALLING REQUIRED PROGRAMS..."
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Installing pip
+echo "INSTALLING pip"
+sudo apt-get install python3-pip -y
+python3 -m pip install --upgrade pip
+
+# Installing curl
+echo "INSTALLING curl"
+sudo apt-get install curl -y
+
+# Installing node using nvm
+echo "INSTALLING node"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+command -v nvm
+nvm install --lts
+nvm ls
+
+# Installing powerline
+echo "INSTALLING powerline"
+pip install powerline-status
+pip install powerline-gitstatus
+
+# Installing Neovim
+echo "INSTALLING Neovim"
+sudo apt-get install fuse libfuse2 ack-grep -y
+wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
+chmod +x nvim
+sudo chown root:root nvim
+sudo mv nvim /usr/bin
+
+# Installing ripgrep
+echo "INSTALLING ripgrep"
+sudo apt-get install ripgrep -y
+
+# Installing stow
+echo "INSTALLING stow"
+sudo apt-get stow -y
